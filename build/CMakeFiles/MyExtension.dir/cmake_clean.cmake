9499eb16ea2f68cc08dc9367b7afa22cbb8ef6d7
FreeroamDevs <freeroam.office@gmail.com>
2026-04-09 00:49:09 +0700

    Linked to Autocad. Everything MUST BE DONE TOMORROW.

---
+++ build/CMakeFiles/MyExtension.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExtension.dir/src/main.cpp.o"
+  "CMakeFiles/MyExtension.dir/src/main.cpp.o.d"
+  "bin/MyExtension.bundle/Contents/MacOS/MyExtension"
+  "bin/MyExtension.bundle/Contents/MacOS/MyExtension.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExtension.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyExtension.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyExtension.
+# This may be replaced when dependencies are built.
